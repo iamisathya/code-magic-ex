1c602150360e7d0268ab2823308d34f88a6e29f0
Sathya <sathyanarayana.an@unicity.com>
2021-06-10 14:14:13 +0530

    Added centeral address

--- lib/api/api_address.dart
+++ lib/api/api_address.dart
@@ -1,3 +1,13 @@
 class Address {
-  static const String BASE_URL =  "https://hydra.unicity.net/v5a/";
+  static const String baseUrl =  "https://hydra.unicity.net/v5a/";
+  static const String loginTokens =  "/loginTokens";
+  static const String customerData =  "/customers/{id}";
+  static const String managedWarehouse =  "/customers/me/managedwarehouses";
+  static const String inventoryRecords =  "/warehouses/{id}/inventoryRecords";
+  static const String inventoryMovementRecords =  "/warehouses/{id}/inventoryMovementRecords";
+  static const String ordersAndRmas =  "/warehouses/{id}/ordersAndRmas";
+  static const String customers =  "/customers";
+  static const String orderTerms =  "/orderTerms";
+  static const String orders =  "/warehouses/{id}/orders";
+  static const String customersPath =  "customers";
 }
