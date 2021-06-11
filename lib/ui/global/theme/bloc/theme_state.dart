6792bfeb6629fb61e1c60944e9e527478fb0d951
Sathya <sathyanarayana.an@unicity.com>
2021-06-11 07:51:19 +0530

    Removed unnnessary folder

--- lib/ui/global/theme/bloc/theme_state.dart
+++
@@ -1,9 +0,0 @@
-// part of 'theme_bloc.dart';
-
-// @immutable
-// abstract class ThemeState extends Equatable{
-//   final ThemeData themeData;
-
-//   ThemeState({required this.themeData}): super([themeData]); 
-// }
-
