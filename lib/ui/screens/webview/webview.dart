7b0e8942f69f595e6d0cbb9f1492e1dfdf0cfb62
Sathya <sathyanarayana.an@unicity.com>
2021-06-16 17:09:47 +0530

    track webview prgress

--- lib/ui/screens/webview/webview.dart
+++ lib/ui/screens/webview/webview.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
@@ -11,9 +13,36 @@ class WebivewHomeScreen extends StatefulWidget {
 }
 
 class _WebivewHomeScreenState extends State<WebivewHomeScreen> {
+  // Instance of WebView plugin
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  late StreamSubscription<double> _onProgressChanged;
+  double currentProgress = 0.0;
+  bool canHideWebview = true;
+
   @override
   void initState() {
     super.initState();
+    _onProgressChanged =
+        flutterWebViewPlugin.onProgressChanged.listen((double progress) {
+      if (mounted) {
+        if(progress == 1.0) {
+          setState(() {
+          canHideWebview = false;
+        });
+        }
+        setState(() {
+          currentProgress = progress;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Every listener should be canceled, the same should be done with this stream.
+    _onProgressChanged.cancel();
+    flutterWebViewPlugin.dispose();
+    super.dispose();
   }
 
   @override
@@ -31,15 +60,10 @@ class _WebivewHomeScreenState extends State<WebivewHomeScreen> {
           ),
         ],
       ),
-      initialChild: Container(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
       withZoom: true,
       useWideViewPort: true,
-      url:
-          'https://dsc-th.unicity.com/invoice.php?link=https://hydra.unicity.net/v5a/orders/31512d2a1d4a2a5860bc785d27d1f7520ff03edcb153eeed5e2d6ab775a39f01&token=fce3ca65-b961-4961-bfce-cd8ffe7e5134',
+      url:'https://facebook.com'
+          // 'https://dsc-th.unicity.com/invoice.php?link=https://hydra.unicity.net/v5a/orders/31512d2a1d4a2a5860bc785d27d1f7520ff03edcb153eeed5e2d6ab775a39f01&token=fce3ca65-b961-4961-bfce-cd8ffe7e5134',
     );
   }
 }
