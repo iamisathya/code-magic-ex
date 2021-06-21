990606bd53e083fdc0204633c070048c29336832
Sathya <sathyanarayana.an@unicity.com>
2021-06-21 11:00:30 +0530

    Main changes

--- lib/ui/global/widgets/default_button.dart
+++ lib/ui/global/widgets/default_button.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:code_magic_ex/utilities/constants.dart';
 import 'package:code_magic_ex/utilities/size_config.dart';
 
-
 class DefaultButton extends StatelessWidget {
   const DefaultButton({
     required this.text,
@@ -15,19 +14,17 @@ class DefaultButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
-      height: getProportionateScreenHeight(56),
-      child: ElevatedButton(
-        style: ButtonStyle(shape: elevatedShape()),
-        onPressed: () => press,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.blueAccent,
-            fontSize: getProportionateScreenWidth(14),
-          ),
-        ),
-      ),
-    );
+        width: double.infinity,
+        height: getProportionateScreenHeight(56),
+        child: ElevatedButton.icon(
+          icon: Text(text,
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: getProportionateScreenWidth(14),
+              )),
+          label: const Icon(Icons.arrow_forward, size: 16, color: Colors.blueAccent),
+          style: ButtonStyle(shape: elevatedShape()),
+          onPressed: () => press(),
+        ));
   }
 }
--- lib/ui/screens/login/components/sign_form.dart
+++ lib/ui/screens/login/components/sign_form.dart
@@ -3,7 +3,6 @@ import 'package:code_magic_ex/ui/screens/home/home.dart';
 import 'package:code_magic_ex/utilities/constants.dart';
 import 'package:code_magic_ex/ui/global/widgets/custom_surfix_icon.dart';
 import 'package:code_magic_ex/ui/global/widgets/default_button.dart';
-import 'package:code_magic_ex/ui/screens/login/components/form_error.dart';
 import 'package:code_magic_ex/utilities/keyboard.dart';
 import 'package:code_magic_ex/utilities/size_config.dart';
 import 'package:flutter/material.dart';
@@ -18,21 +17,13 @@ class _SignFormState extends State<SignForm> {
   late String email;
   late String password;
   bool remember = false;
-  final List<String> errors = [];
 
-  void addError({required String error}) {
-    if (!errors.contains(error)) {
-      setState(() {
-        errors.add(error);
-      });
-    }
-  }
-
-  void removeError({required String error}) {
-    if (errors.contains(error)) {
-      setState(() {
-        errors.remove(error);
-      });
+  void _onPressContinue() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // if all are valid then go to success screen
+      KeyboardUtil.hideKeyboard(context);
+      Navigator.pushNamed(context, MainHomeScreen.routeName);
     }
   }
 
@@ -42,7 +33,7 @@ class _SignFormState extends State<SignForm> {
       key: _formKey,
       child: Column(
         children: [
-          buildEmailFormField(),
+          buildUserIdFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
           buildPasswordFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
@@ -68,18 +59,10 @@ class _SignFormState extends State<SignForm> {
               )
             ],
           ),
-          FormError(errors: errors),
           SizedBox(height: getProportionateScreenHeight(20)),
           DefaultButton(
             text: "Continue",
-            press: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-                // if all are valid then go to success screen
-                KeyboardUtil.hideKeyboard(context);
-                Navigator.pushNamed(context, MainHomeScreen.routeName);
-              }
-            },
+            press: () => _onPressContinue(),
           ),
         ],
       ),
@@ -90,30 +73,18 @@ class _SignFormState extends State<SignForm> {
     return TextFormField(
       obscureText: true,
       onSaved: (newValue) => password = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPassNullError);
-        } else if (value.length >= 8) {
-          removeError(error: kShortPassError);
-        }
-        return null;
-      },
       validator: (value) {
         if (value!.isEmpty) {
-          addError(error: kPassNullError);
-          return "";
-        } else if (value.length < 8) {
-          addError(error: kShortPassError);
-          return "";
-        }
+          return kPassNullError;
+        } 
         return null;
       },
       decoration: const InputDecoration(
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(color: Colors.blueGrey),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.blueAccent),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey),
         ),
         labelStyle: TextStyle(color: Colors.grey),
         labelText: "Password",
@@ -126,34 +97,21 @@ class _SignFormState extends State<SignForm> {
     );
   }
 
-  TextFormField buildEmailFormField() {
+  TextFormField buildUserIdFormField() {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       onSaved: (newValue) => email = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kEmailNullError);
-        } else if (emailValidatorRegExp.hasMatch(value)) {
-          removeError(error: kInvalidEmailError);
-        }
-        return null;
-      },
       validator: (value) {
         if (value!.isEmpty) {
-          addError(error: kEmailNullError);
-          return "";
-        } else if (!emailValidatorRegExp.hasMatch(value)) {
-          addError(error: kInvalidEmailError);
-          return "";
+          return kUserIdNullError;
         }
-        return null;
       },
       decoration: const InputDecoration(
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(color: Colors.blueGrey),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.blueAccent),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey),
         ),
         labelStyle: TextStyle(color: Colors.grey),
         labelText: "User ID",
