7334f7c1d91c5ba0553ddef4e55fe7c77e417eb8
Sathya <sathyanarayana.an@unicity.com>
2021-07-22 13:44:35 +0530

    change

--- lib/ui/global/widgets/transparent_app_bar.dart
+++ lib/ui/global/widgets/transparent_app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:code_magic_ex/utilities/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class TransAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -17,18 +16,15 @@ class TransAppBar extends StatelessWidget implements PreferredSizeWidget {
         iconTheme: const IconThemeData(color: kMainColor),
         backgroundColor: Colors.white,
         elevation: 0,
-        title: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(color: kMainColor),
-              ),
-              if (subTitle != "")
-                Text(subTitle,
-                    style: const TextStyle(color: kMainColor, fontSize: 14)),
-            ]));
+        title: Column(children: [
+          Text(
+            title,
+            style: const TextStyle(color: kMainColor),
+          ),
+          if (subTitle != "")
+            Text(subTitle,
+                style: const TextStyle(color: kMainColor, fontSize: 14)),
+        ]));
   }
 
   @override
