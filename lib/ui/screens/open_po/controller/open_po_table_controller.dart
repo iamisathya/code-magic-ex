6cbfb1a2cad7122b550b1e94af3cb1aac6df694a
Sathya <sathyanarayana.an@unicity.com>
2021-07-27 12:58:17 +0530

    Updated order table

--- lib/ui/screens/open_po/controller/open_po_table_controller.dart
+++ lib/ui/screens/open_po/controller/open_po_table_controller.dart
@@ -1,18 +1,11 @@
-import 'dart:convert';
-
 import 'package:code_magic_ex/api/config/api_service.dart';
 import 'package:code_magic_ex/api/config/member_class.dart';
-import 'package:code_magic_ex/api/request/request_order_calculation.dart';
-import 'package:code_magic_ex/models/order_calc_response.dart';
 import 'package:code_magic_ex/models/cart_products.dart';
-import 'package:code_magic_ex/models/cash_coupon_response.dart';
-import 'package:code_magic_ex/models/enroll_response.dart';
 import 'package:code_magic_ex/models/inventory_records.dart';
-import 'package:code_magic_ex/models/radio_button_value.dart';
 import 'package:code_magic_ex/models/user_info.dart';
 import 'package:code_magic_ex/models/user_minimal_data.dart';
+import 'package:code_magic_ex/models/validate_order.dart';
 import 'package:code_magic_ex/ui/global/widgets/overlay_progress.dart';
-import 'package:code_magic_ex/ui/screens/order_entry/screens/checkout/checkout_screen.dart';
 import 'package:code_magic_ex/utilities/constants.dart';
 import 'package:code_magic_ex/utilities/enums.dart';
 import 'package:code_magic_ex/utilities/function.dart';
@@ -31,22 +24,6 @@ class OpenPoTableController extends GetxController {
   RxBool isLoading = false.obs;
   RxDouble totalCartPrice = 0.0.obs;
   RxInt totalCartPv = 0.obs;
-  RxInt availableCreditAmount = 0.obs;
-  RxDouble totalCheckoutAmount = 0.0.obs;
-  RxList<RadioButtonModel> paymentOptions = [
-    RadioButtonModel(
-      index: 0,
-      name: "DSC",
-    ),
-    RadioButtonModel(
-      index: 1,
-      name: "Voucher",
-    ),
-  ].obs;
-  Rx<RadioButtonModel> seletedOption = RadioButtonModel(
-    index: 0,
-    name: "DSC",
-  ).obs;
 
   final ProgressBar _sendingMsgProgressBar = ProgressBar();
   late UserMinimalData passedUser;
@@ -95,89 +72,6 @@ class OpenPoTableController extends GetxController {
     }
   }
 
-  Future<bool> validateEmail(BuildContext context) async {
-    try {
-      // ! Check this api parsing
-      final dynamic result = await MemberCallsService.init()
-          .validateEmail(kCurrentLanguage, "rasachankan@gmail.com");
-      final jsonResponse = jsonDecode(result.toString());
-      final EnrollResponse enrollResponse =
-          EnrollResponse.fromJson(jsonResponse as Map<String, dynamic>);
-      if (enrollResponse.success != "No") {
-        return true;
-      }
-      return false;
-    } on DioError catch (e) {
-      _onDioError(e);
-      return false;
-    } catch (err) {
-      _onCatchError(err);
-      return false;
-    } finally {
-      isLoading(false);
-      update();
-    }
-  }
-
-  Future<bool> orderCalculation(BuildContext context) async {
-    try {
-      final nonEmptyProducts = cartProducts.where((e) => e.itemCode != "");
-      final List<LineItem> checkoutItems = nonEmptyProducts
-          .map((element) => LineItem(
-              itemId: element.itemCode,
-              quantity: element.quantity.toString(),
-              items: CustomerHref(
-                  href:
-                      "https://hydra.unicity.net/v5a/items?id.unicity=${element.itemCode}")))
-          .toList();
-      final RequestOrderCalculation requestObject = RequestOrderCalculation(
-          uShopData: UShopData(loginId: "3011266"),
-          order: Order(
-              lines: Lines(items: checkoutItems),
-              shipToAddress: ShipToAddress(country: "TH"),
-              shippingMethod: ShippingMethod(
-                  href:
-                      "https://hydra.unicity.net/v5a/warehouses/9e41f330617aa2801b45620f8ffc5615306328fa0bd2255b0d42d7746560d24c/shippingmethods?type=WillCall"),
-              customer: CustomerHref(
-                  href:
-                      "https://hydra.unicity.net/v5a/customers?type=Associate")));
-      final OrderCalculationResponse validationResponse =
-          await MemberCalls2Service.init().orderCalculation(requestObject);
-      if (validationResponse.items.isNotEmpty) {
-        return true;
-      }
-      return false;
-    } on DioError catch (e) {
-      _onDioError(e);
-      return false;
-    } catch (err) {
-      _onCatchError(err);
-      return false;
-    } finally {
-      isLoading(false);
-      update();
-    }
-  }
-
-  Future<void> getCashCoupon(BuildContext context) async {
-    try {
-      final CashCouponResponse cashCoupon = await MemberCalls2Service.init()
-          .getCashCoupon(totalCartPv.value.toString());
-      if (cashCoupon.success != false) {
-        // continue with order place
-        availableCreditAmount.value = cashCoupon.availableCreditAmount;
-      }
-      onChangedSearchType(seletedOption.value);
-    } on DioError catch (e) {
-      _onDioError(e);
-    } catch (err) {
-      _onCatchError(err);
-    } finally {
-      isLoading(false);
-      update();
-    }
-  }
-
   Future<void> validateOrder(BuildContext context) async {
     checkoutProducts.value =
         cartProducts.where((el) => el.itemCode != "").toList();
@@ -187,21 +81,54 @@ class OpenPoTableController extends GetxController {
           subTitle: "Please select products to proceed with checkout!");
       return;
     }
+
     _sendingMsgProgressBar.show(context);
-    final bool isValidEMail = await validateEmail(context);
-    if (!isValidEMail) return;
-
-    final bool validated = await orderCalculation(context);
-    if (!validated) return;
-
-    await getCashCoupon(context);
-    _sendingMsgProgressBar.hide();
-    // ! Check this warnig message comming
-    Get.to(() => CheckoutPage(), transition: Transition.downToUp);
+    try {
+      final ValidateOrder reponse =
+          await MemberCallsService.init().valiadateOrder("TH", "BKM");
+      if (reponse.success == "yes") {
+        // placeOrder(reponse.message);
+      }
+      _sendingMsgProgressBar.hide();
+    } catch (err) {
+      _sendingMsgProgressBar.hide();
+      errorMessage(err.toString());
+      LoggerService.instance.e(err.toString());
+    }
   }
 
-  String findItemCode(int idx) {
-    return inventoryRecords.value.items[idx].item.id.unicity;
+  Future<void> placeOrder(String orderId) async {
+    // type: 201, comment: TEST ORDER 2, token: cyzr29ke2go0at89ygorpdd, cus_id: 1, cus_dscid: 0001, poid: BKM 2021-07-W002, totalpv: 20, totalprice: 7,900, cusname: Thailand TEST DSC, data: @17532|1@17616|1
+    try {
+      final dynamic reponse = await MemberCallsService.init().placeOrder(
+          kPlaceOrder,
+          "TEST ORDER DYNAMIC",
+          generateRandomString(23),
+          UserSessionManager.shared.customerId,
+          UserSessionManager.shared.customerCode,
+          orderId,
+          totalCartPv.value.toString(),
+          totalCartPrice.value.toString(),
+          "Thailand TEST DSC",
+          _collectOrderData());
+      if (reponse.success == "yes") {}
+    } catch (err) {
+      errorMessage(err.toString());
+      LoggerService.instance.e(err.toString());
+      update();
+    }
+  }
+
+  String _collectOrderData() {
+    final buffer = StringBuffer();
+    try {
+      for (final item in cartProducts) {
+        buffer.write("@${item.itemCode}|${item.quantity}");
+      }
+      return buffer.toString();
+    } catch (e) {
+      return buffer.toString();
+    }
   }
 
   void addItemToCart({required String itemCode, required int index}) {
@@ -268,19 +195,10 @@ class OpenPoTableController extends GetxController {
     calculateTotal();
   }
 
-  void placeOrder() {}
-
   List<String> dropDownItems() {
     final items =
         inventoryRecords.value.items.map((e) => e.item.id.unicity).toList();
     return items;
-  }
-
-  void onChangedSearchType(RadioButtonModel data) {
-    seletedOption.value = paymentOptions[data.index];
-    totalCheckoutAmount.value = data.index == 0
-        ? totalCartPrice.value
-        : totalCartPrice.value - availableCreditAmount.value;
   }
 
   void _onDioError(DioError e) {
