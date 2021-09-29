12fa77f137caa296098b438dbaca61812038e351
Sathya <sathyanarayana.an@unicity.com>
2021-09-30 00:21:04 +0530

    button update

--- lib/ui/global/widgets/plain_button.dart
+++ lib/ui/global/widgets/plain_button.dart
@@ -6,12 +6,14 @@ class PlainButton extends StatelessWidget {
   final String title;
   final Color titleColor;
   final Color buttonColor;
+  final bool showIcons;
   final VoidCallback? onTap;
 
   const PlainButton(
       {required this.title,
       this.titleColor = const Color(0xFFFFFFFF),
       this.buttonColor = const Color(0xFF1C9CFC),
+      this.showIcons = false,
       this.onTap});
 
   @override
@@ -26,14 +28,14 @@ class PlainButton extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(15),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: showIcons ? MainAxisAlignment.spaceBetween : MainAxisAlignment.center, 
             children: [
               Text(title,
                   style: Theme.of(context)
                       .textTheme
                       .bodyText1!
                       .copyWith(color: titleColor)),
-              SvgPicture.asset(kForwardIcon, width: 34, height: 10)
+              if(showIcons) SvgPicture.asset(kForwardIcon, width: 34, height: 10)
             ],
           ),
         ),
