9aa88cf2144564f65d2ff6337f8eea718b8440b0
Sathya <sathyanarayana.an@unicity.com>
2021-11-18 19:04:13 +0530

    small changes

--- lib/ui/screens/enroll/screens/enrollment_summary/components/body.dart
+++ lib/ui/screens/enroll/screens/enrollment_summary/components/body.dart
@@ -1,6 +1,5 @@
 import 'package:dsc_tools/models/cart_products.dart';
 import 'package:dsc_tools/ui/screens/enroll/controllers/enroll.controller.dart';
-import 'package:dsc_tools/ui/screens/enroll/screens/enroll/components/enroll_cart_item.dart';
 import 'package:dsc_tools/ui/screens/enroll/screens/enrollment_summary/controller/enrollment.summary.controller.dart';
 import 'package:dsc_tools/ui/screens/inventory/component/page_title.dart';
 import 'package:flutter/material.dart';
@@ -34,22 +33,24 @@ class Body extends StatelessWidget {
             const PageTitle(title: "Enrollment Summary"),
             Container(
               padding: const EdgeInsets.all(20),
+              child: Obx(
+                () => ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: enrollHomeController.cartProducts.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    final CartProductsItem item =
+                        enrollHomeController.cartProducts[index];
+                    return EnrollSummaryCartItem(item: item, idx: index);
+                  },
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
               color: const Color(0xFFF6F9FD),
               child: Column(
                 children: [
-                  Obx(
-                    () => ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: enrollHomeController.cartProducts.length,
-                      itemBuilder: (BuildContext ctxt, int index) {
-                        final CartProductsItem item =
-                            enrollHomeController.cartProducts[index];
-                        return EnrollSummaryCartItem(item: item, idx: index);
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20),
                   ShippingAddressCard(),
                   const SizedBox(height: 20),
                   EnrollerInfoCard(),
