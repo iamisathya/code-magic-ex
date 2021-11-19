93b19f855988a8c747638106d259333a02593ff0
Sathya <sathyanarayana.an@unicity.com>
2021-11-19 14:53:47 +0530

    Added terms & conditons

--- lib/ui/screens/enroll/screens/enrollement_condition/components/body.dart
+++ lib/ui/screens/enroll/screens/enrollement_condition/components/body.dart
@@ -1,17 +1,35 @@
+import 'package:dsc_tools/constants/globals.dart';
+import 'package:dsc_tools/ui/global/theme/text_view.dart';
+import 'package:dsc_tools/utilities/constants.dart';
+import 'package:dsc_tools/utilities/enums.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../inventory/component/page_title.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final Map<String, dynamic>? found = termsAndConditions.firstWhereOrNull((market) =>
+        market["countryCode"] == Globals.countryCode);
+    final List<String> tems = found == null ? [] : found["content"][Globals.defaultLanguage.toUpperCase() != "EN" ? "native" : "english"] as List<String>;
     return SingleChildScrollView(
       child: SafeArea(
           child: Column(
-        children: const [
-          PageTitle(title: "Terms & conditions"),
+        children: [
+          const PageTitle(title: "Terms & conditions"),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: tems
+                    .map((e) => Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: AppText(text: e, style: TextTypes.bodyText1),
+                        ))
+                    .toList()),
+          ),
         ],
       )),
     );
--- lib/ui/screens/enroll/screens/enrollment_details/controller/enrollement.details.controller.dart
+++ lib/ui/screens/enroll/screens/enrollment_details/controller/enrollement.details.controller.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dsc_tools/ui/screens/enroll/screens/enrollement_condition/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
@@ -34,7 +35,7 @@ class EnrollmentDetailsHomeController extends GetxController {
   RxInt isValidSponsorId = 0.obs;
 
   void navigateToTermsPage() {
-    Get.to(() => EnrollmentTermsController());
+    Get.to(() => EnrollmentTermsScreen());
   }
 
   Future<void> verifyUserInfo() async {
