e475ddbafdea688d59b603e3376b094dc259b20b
Sathya <sathyanarayana.an@unicity.com>
2022-02-07 14:11:05 +0530

    ⚡️ 🎨 Small loading indicator inside button

--- lib/ui/global/widgets/raised_gradient_button.dart
+++ lib/ui/global/widgets/raised_gradient_button.dart
@@ -1,6 +1,8 @@
 import 'package:dsc_tools/constants/colors.dart';
 import 'package:dsc_tools/ui/global/theme/text_view.dart';
+import 'package:dsc_tools/ui/screens/open_po/order_create/component/loader.dart';
 import 'package:dsc_tools/utilities/enums.dart';
+import 'package:dsc_tools/utilities/images.dart';
 import 'package:flutter/material.dart';
 
 class RaisedGradientButton extends StatelessWidget {
@@ -8,6 +10,7 @@ class RaisedGradientButton extends StatelessWidget {
   final double width;
   final double height;
   final String label;
+  final bool isLoading;
   final Function onPressed;
 
   const RaisedGradientButton({
@@ -16,6 +19,7 @@ class RaisedGradientButton extends StatelessWidget {
     this.width = double.infinity,
     this.height = 54.0,
     this.label = "Login",
+    this.isLoading = false,
     required this.onPressed,
   }) : super(key: key);
 
@@ -30,12 +34,29 @@ class RaisedGradientButton extends StatelessWidget {
         color: Colors.transparent,
         child: InkWell(
             onTap: () => onPressed(),
-            child: Center(
-              child: AppText(
-                text: label,
-                style: TextTypes.bodyText1,
-                color: AppColor.kWhiteColor,
-              ),
+            child: Stack(
+              children: [
+                Center(
+                  child: AppText(
+                    text: label,
+                    style: TextTypes.bodyText1,
+                    color: AppColor.kWhiteColor,
+                  ),
+                ),
+                if(isLoading)
+                Align(
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    margin: const EdgeInsets.only(left: 150),
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(kAnimatedSpin),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             )),
       ),
     );
--- lib/ui/screens/login/components/body.dart
+++ lib/ui/screens/login/components/body.dart
@@ -17,79 +17,82 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 60),
-          SvgPicture.asset(kUnicityLogoGradientImage, height: 25),
-          const SizedBox(height: 60),
-          SvgPicture.asset(kLoginScreenImage),
-          const SizedBox(height: 60),
-          AppText(text: "make_life_better".tr, style: TextTypes.headline4),
-          const SizedBox(height: 15),
-          GestureDetector(
-            onTap: _controller.onClickForgotPassword,
-            child: AppText(
-                text: "forgot_password".tr,
-                style: TextTypes.bodyText1,
-                color: AppColor.dodgerBlue),
-          ),
-          // SizedBox(height: SizeConfig.screenHeight * 0.04),
-          const SizedBox(height: 40),
-          Container(
-            color: AppColor.ateneoBlue,
-            padding: const EdgeInsets.all(40),
-            width: double.infinity,
-            child: Form(
-              key: _controller.formKey,
-              child: Obx(
-                () => Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    LoginTextField(
-                        controller: _controller.userIdController,
-                        textInputAction: TextInputAction.next,
-                        hintText: "Username"),
-                    const SizedBox(height: 20),
-                    LoginTextField(
-                        controller: _controller.passwordController,
-                        secureText: true,
-                        textInputAction: TextInputAction.go,
-                        hintText: "Password"),
-                        const SizedBox(height: 20),
-                    RaisedGradientButton(
-                      gradient: const LinearGradient(
-                        colors: <Color>[Color(0xFF1C9CFC), Color(0xFF4CDFFF)],
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            SvgPicture.asset(kUnicityLogoGradientImage, height: 25),
+            const SizedBox(height: 60),
+            SvgPicture.asset(kLoginScreenImage),
+            const SizedBox(height: 60),
+            AppText(text: "make_life_better".tr, style: TextTypes.headline4),
+            const SizedBox(height: 15),
+            GestureDetector(
+              onTap: _controller.onClickForgotPassword,
+              child: AppText(
+                  text: "forgot_password".tr,
+                  style: TextTypes.bodyText1,
+                  color: AppColor.dodgerBlue),
+            ),
+            // SizedBox(height: SizeConfig.screenHeight * 0.04),
+            const SizedBox(height: 40),
+            Container(
+              color: AppColor.ateneoBlue,
+              padding: const EdgeInsets.all(40),
+              width: double.infinity,
+              child: Form(
+                key: _controller.formKey,
+                child: Obx(
+                  () => Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      LoginTextField(
+                          controller: _controller.userIdController,
+                          textInputAction: TextInputAction.next,
+                          hintText: "Username"),
+                      const SizedBox(height: 20),
+                      LoginTextField(
+                          controller: _controller.passwordController,
+                          secureText: true,
+                          textInputAction: TextInputAction.go,
+                          hintText: "Password"),
+                          const SizedBox(height: 20),
+                      RaisedGradientButton(
+                        gradient: const LinearGradient(
+                          colors: <Color>[Color(0xFF1C9CFC), Color(0xFF4CDFFF)],
+                        ),
+                        onPressed: () => _controller.onPressContinue(context),
+                        label: 'login_to_dsc',
+                        isLoading: _controller.loading.value,
                       ),
-                      onPressed: () => _controller.onPressContinue(context),
-                      label: 'login_to_dsc',
-                    ),
-                    const SizedBox(height: 10),
-                    if (_controller.errorMessages.isNotEmpty)
-                      ErrorMessage(errors: [_controller.errorMessages.value]),
-                  ],
+                      const SizedBox(height: 10),
+                      if (_controller.errorMessages.isNotEmpty)
+                        ErrorMessage(errors: [_controller.errorMessages.value]),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Obx(() => _controller.isSessionExpired.value
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  child: AppText(
-                      text: "session_expired_login_again".tr,
-                      align: TextAlign.center,
-                      style: TextTypes.subtitle1,
-                      color: Colors.red),
-                )
-              : const SizedBox()),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: AppText(
-                text: "copyright_message".tr,
-                style: TextTypes.bodyText1,
-                color: AppColor.manatee),
-          ),
-        ],
-      ),
+            Obx(() => _controller.isSessionExpired.value
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: AppText(
+                        text: "session_expired_login_again".tr,
+                        align: TextAlign.center,
+                        style: TextTypes.subtitle1,
+                        color: Colors.red),
+                  )
+                : const SizedBox()),
+            const SizedBox(height: 26),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: AppText(
+                  text: "copyright_message".tr,
+                  style: TextTypes.bodyText1,
+                  color: AppColor.manatee),
+            ),
+          ],
+        ),
+      
     );
   }
 }
